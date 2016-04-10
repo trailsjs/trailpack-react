6d47cba375cbdc45bc4c33260d07c807c3a3739f
Weyland Joyner <weylandjoyner4@gmail.com>
2016-04-09 21:49:59 -0400

    added loaders for other filetypes

--- archetype/config/webpack.js
+++ archetype/config/webpack.js
@@ -17,11 +17,23 @@ module.exports = {
     module: {
       loaders: [{
         test: /\.js$/,
-        loader: 'babel-loader',
+        loader: 'babel',
         query: {
           presets: ['react', 'es2015', 'stage-0']
-        },
-        include: path.join(__dirname, '../../../src'),
+        }
+      }, {
+        test: /\.scss$/,
+        loaders: [
+          'style',
+          'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+          'sass'
+        ]
+      }, {
+        test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'file-loader?name=fonts/[name].[ext]'
+      }, {
+        test: /\.(png|jpg)$/,
+        loader: 'url?limit=25000'
       }]
     }
   }
